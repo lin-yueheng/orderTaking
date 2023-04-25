ea62127bfeb21a93c22aa31af8c60b9046b8236c
RP <2390245986@qq.com>
2023-04-25 15:33:26 +0800

    分支1 第一次提交

--- src/main/java/com/dazuoye/xiaoyuansaishi1/Xiaoyuansaishi1Application.java
+++ src/main/java/com/dazuoye/xiaoyuansaishi1/Xiaoyuansaishi1Application.java
@@ -12,7 +12,7 @@ public class Xiaoyuansaishi1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Xiaoyuansaishi1Application.class, args);
-        log.info("success");
+        log.info("success，项目启动成功^_^");
     }
 
 }
